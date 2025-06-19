077d8d57b700c85420ca630d2d4626b0775ba6b7
N.H.M.Dilhara <2022t01525@stu.cmb.ac.lk>
2025-06-19 12:13:40 +0530

    small error fixed

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ProfileActivity extends Activity {
     @Override
@@ -24,6 +25,12 @@ public class ProfileActivity extends Activity {
             }
         });
 
+        // Reference to the user info TextView
+        final TextView userInfoText = findViewById(R.id.userInfoText);
+        // Store current values
+        final String[] currentUsername = {"N.H.M.Dilhara"};
+        final String[] currentEmail = {"2022t01525@stu.cmb.ac.lk"};
+
         Button editInfoButton = findViewById(R.id.editInfoButton);
         editInfoButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,13 +41,22 @@ public class ProfileActivity extends Activity {
                 builder.setView(dialogView);
                 AlertDialog dialog = builder.create();
 
+                EditText editUsername = dialogView.findViewById(R.id.editUsername);
+                EditText editEmail = dialogView.findViewById(R.id.editEmail);
+                editUsername.setText(currentUsername[0]);
+                editEmail.setText(currentEmail[0]);
+
                 Button okButton = dialogView.findViewById(R.id.okButton);
                 Button cancelButton = dialogView.findViewById(R.id.cancelButton);
 
                 okButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        // Save info logic here if needed
+                        String newUsername = editUsername.getText().toString();
+                        String newEmail = editEmail.getText().toString();
+                        currentUsername[0] = newUsername;
+                        currentEmail[0] = newEmail;
+                        userInfoText.setText("\n    Username : " + newUsername + "\n\n    E-mail :\n    " + newEmail);
                         dialog.dismiss();
                     }
                 });
