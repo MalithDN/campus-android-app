539ba643a8bf33f2fa3d4b4b42a62121f9401bda
N.H.M.Dilhara <2022t01525@stu.cmb.ac.lk>
2025-06-19 11:13:26 +0530

    Add profile interface back button

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,9 +1,11 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class ProfileActivity extends Activity {
     @Override
@@ -11,5 +13,15 @@ public class ProfileActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         // You can add button listeners here if needed
+        ImageView backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, DashboardActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 } 
